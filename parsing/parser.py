122f17c6b9c3fd2f7adcd5e41e6952fcc3eb6ec0
CriptoMikael <mikhailin98@yandex.ru>
2020-10-17 03:39:22 +0300

    Refactor code.

--- parsing/parser.py
+++ parsing/parser.py
@@ -1,22 +1,32 @@
 import json
 
 
-def read_file():
-    with open('input.json', 'r') as f:
+def read_file(path_file):
+    with open(path_file, 'r') as f:
         return json.loads(f.read())
 
 
-def get_adjacency_matrix(works, edjes):
+def get_adjacency_matrix(works, edges):
     arr = list()
-    for i in edjes:
-        arr.append(tuple((i['start']-1, i['finish']-1, works[i['start']-1]['normal_len'])))
+    for i in edges:
+        if i['start']-1 >= len(works):
+            arr.append(tuple((i['start'] - 1, i['finish'] - 1, 0)))
+        else:
+            arr.append(tuple((i['start']-1, i['finish']-1, works[i['start']-1]['normal_len'])))
     return arr
 
 
 def get_input_data():
-    all_file = read_file()
+    all_file = read_file('input.json')
     works = all_file['works']
     milestones = all_file['milestones']
-    edjes = all_file['edjes']
-    adj_matrix = get_adjacency_matrix(works, edjes)
+    edges = all_file['edges']
+    adj_matrix = get_adjacency_matrix(works, edges)
     return works, milestones, adj_matrix
+
+
+def get_edit_data():
+    all_file = read_file('edit.json')
+    works = all_file['works']
+    milestones = all_file['milestones']
+    return works, milestones
