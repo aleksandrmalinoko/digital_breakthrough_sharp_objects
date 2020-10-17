ccff7ccc4e878e373686355dd8591588d248b5c2
CriptoMikael <mikhailin98@yandex.ru>
2020-10-17 19:14:03 +0300

    Global update.

--- draw/drawgraph.py
+++ draw/drawgraph.py
@@ -1,17 +1,29 @@
 import plotly.express as px
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import networkx as nx
+
 
 def test():
     df = pd.DataFrame([
         dict(Task="Job A", Start='2009-01-01', Finish='2009-02-28', Resource="Alex"),
         dict(Task="Job B", Start='2009-03-05', Finish='2009-04-15', Resource="Alex"),
-        dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30', Resource="Max")
+        dict(Task="Job C", Start='2009-02-20', Finish='2009-05-30', Resource="Max"),
+        dict(Task="Job D", Start='2009-02-20', Finish='2009-05-30', Resource="Max"),
+        dict(Task="Job E", Start='2009-02-20', Finish='2009-05-30', Resource="Max"),
+        dict(Task="Job F", Start='2009-02-20', Finish='2009-05-30', Resource="Max")
     ])
 
     fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource")
     fig.update_yaxes(autorange="reversed")
     fig.show()
+
+
+def draw_graph(dg):
+    plt.subplot(121)
+    nx.draw(dg, with_labels=True, font_weight='bold')
+    plt.show()
 
 '''
 import matplotlib.pyplot as plt
--- main/main.py
+++ main/main.py
@@ -1,6 +1,7 @@
 import os.path
 from parsing import parser
 from draw import drawgraph
+import networkx as nx
 
 
 def main(inputFile, editFile, date, maxPrice):
@@ -9,11 +10,24 @@ def main(inputFile, editFile, date, maxPrice):
     if os.path.isfile(editFile) is False:
         return
 
-    print(parser.get_input_data(inputFile))
+    works, milestones, adj_matrix = parser.get_input_data(inputFile)
+
+    '''print(parser.get_input_data(inputFile))
     print("-----------------")
-    print(parser.get_edit_data(editFile))
+    print(parser.get_edit_data(editFile))'''
 
     drawgraph.test()
+
+    DG = nx.DiGraph()
+    # works, milestones, adj_matrix = get_input_data()
+    DG.add_weighted_edges_from(adj_matrix)
+
+    # y = find_stock_vertex(DG)
+    # x = find_source_vertex(DG)
+    drawgraph.draw_graph(DG)
+    # DG.nodes()
+
+    # test = partitioning_graph(DG, milestones)
 
 
     '''works, milestones, adj_matrix = parser.get_input_data(inputFile)
